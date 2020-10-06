6c98130c9c17398c46b139fa32b2b8cf05412774
Will-Gon <will.colocho@gmail.com>
2020-10-06 11:02:14 -0700

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,24 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+  function greet(time) {
+  let now = time.split(":")
+  let later = now[0].concat(now[1])
+  let hour = parseInt(later)
+
+  if (hour < 1200) {
+    return "Good Morning"
+  }
+  else if ((hour >= 1200) && (hour <= 1700)) {
+    return "Good Afternoon"
+  }
+  else (hour > 1700); {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(message) {
+  document.getElementById('greeting').innerText = message
+}
+
+/*%R - 24-hour time (%H:%M) */
